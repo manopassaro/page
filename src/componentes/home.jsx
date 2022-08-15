e9f0500a05bb6f64cb3e3cc3f09f36337a978649
manopassaro <103702640+manopassaro@users.noreply.github.com>
2022-08-15 17:50:54 -0300

    build: building

---
+++ src/componentes/home.jsx
@@ -0,0 +1,10 @@
+
+export default function () {
+    return (
+        <>
+        <h1>Lista de afazeres</h1>
+        <input type="text" id="task"></input>
+        <button >+</button>
+        </>
+    )
+}
